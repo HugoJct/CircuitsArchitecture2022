2df03b1d53076d4a245be4783d2a33ed8efc1205
Hugo <hjacotot@free.fr>
2022-12-04 00:07:37 +0100

    Added asm file

---
+++ LC-3/Assembly/index.asm
@@ -0,0 +1,31 @@
+.ORIG x3000
+
+;setup
+	LEA R1, str
+	LD R2, char 
+
+;start
+	NOT R2, R2
+	ADD R2, R2, 1	; opposé de char
+
+loop:	LDR R3, R1, 0	;chargement du caractère
+
+	BRnp skip	;si pas fin de chaîne -> skip
+
+	AND R0, R0, 0
+	BR end
+
+skip:	ADD R3, R3, R2
+	BRz skip2
+	ADD R1, R1, 1
+	BR loop
+
+skip2:
+	AND R0, R0, 0
+	ADD R0, R0, R1
+end:	NOP
+
+str: .STRINGZ "Hello World !"
+char: .FILL x57
+	
+.END
