16c0f425372aff1a573c0a196e767c87831c67c3
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-12-06 21:02:24 +0100

    rindex amélioratio,

--- LC-3/Assembly/rindex.asm
+++ LC-3/Assembly/rindex.asm
@@ -7,11 +7,11 @@ LD R2, char
 ;; start
 NOT R2, R2
 ADD R2, R2, 1		; opposé de char
+AND R0, R0, 0
 
 loop:
 	LDR R3, R1, 0	; chargement du caractère
 	BRnp skip		; si pas fin de chaîne -> skip
-	AND R0, R0, 0
 	BR end
 
 skip:	
@@ -23,16 +23,8 @@ skip:
 addCharIndex:
 	AND R0, R0, 0
 	ADD R0, R0, R1
-
-loop2:
 	ADD R1, R1, 1
-	LDR R3, R1, 0	; chargement du caractère
-	BRz end			; si fin de chaîne -> end
-	
-skip2:
-	ADD R3, R3, R2
-	BRz addCharIndex
-	BR loop2
+	BR loop
 
 end: NOP
 
