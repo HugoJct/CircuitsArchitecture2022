679db45fded666a7527c886a16ba1fbbfd13a8d9
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-12-15 16:45:55 +0100

    strcmp fixed

--- LC-3/Assembly/strcmp.asm
+++ LC-3/Assembly/strcmp.asm
@@ -3,54 +3,37 @@
 ;; @param R0 registre de destination
 ;; @param R1 adresse début str1
 ;; @param R2 adresse début str2
-;; @param R3 buffer pour str1
-;; @param R4 buffer pour str2
+;; @param R3 char pour str1
+;; @param R4 char pour str2
 
 ;; setup
 LEA R1, str1
 LEA R2, str2
 
-strlen:
+loop:
     LDR R3, R1, 0       ; chargement caractère str1
-    BRz str1Diff        ; si fin str1 on va compter la différence avec str2
-    ADD R1, R1, 1       ; sinon on incrémente l'adresse de str1
-    
+    BRz end
     LDR R4, R2, 0       ; chargement caractère str2
-    BRz str2Diff        ; si fin str2 on va compter la différence avec str1
-    ADD R2, R2, 1       ; sinon on incrémente l'adresse de str2
-    BR strlen
+    NOT R4, R4          ; opposé du caractère chargé
+    ADD R4, R4, 1       ; pour soustraire au caractère de str1
+    BRz end
 
-str1Diff:
-    AND R3, R3, 0       ; on réinitialise le buffer de R1
-str1DiffLoop:
-    LDR R4, R2, 0       ; chargement caractère str2
-    BRz end1            ; si fin str2 on va compter la différence
-    ADD R2, R2, 1       ; sinon on incrémente l'adresse de str2
-    ADD R3, R3, 1       ; et on incrémente le buffer de str1
-    BR str1DiffLoop
+skip:
+    ADD R3, R3, R4      ; char1 - char2
+    BRnp skip2          ; si différent de 0, caractères différents 
+    ADD R1, R1, 1       ; incrémentation str1
+    ADD R2, R2, 1       ; incrémentation str2
+    BR loop
 
-str2Diff:
-    AND R4, R4, 0       ; on réinitialise le buffer de R2
-str2DiffLoop:
-    LDR R3, R1, 0       ; chargement caractère str1
-    BRz end2            ; si fin str2 on va compter la différence
-    ADD R1, R1, 1       ; sinon on incrémente l'adresse de str1
-    ADD R4, R4, 1       ; et on incrémente le buffer de str2
-    BR str2DiffLoop
-
-end1 : 
-    AND R0, R0, 0       ; on initialise de registre de destination pour le retour
-    ADD R0, R0, R3      ; on met dans R0 la valeur de retour
-    BR ret
-end2 :
-    AND R0, R0, 0       ; on initialise de registre de destination pour le retour
-    ADD R0, R0, R4      ; on met dans R0 la valeur de retour
-    BR ret
-
-ret:
+skip2:
+    AND R0, R0, 0       ; initialisation registre de retour
+    AND R0, R0, R3      ; ajout de la différence entre char1 et char2 dans R0
     RET
 
+end : 
+    AND R0, R0, 0       ; on initialise de registre de destination pour le retour
+
 str1: .STRINGZ "Hello World"
-str2: .STRINGZ "Hello"
+str2: .STRINGZ "Healo"
 
 .END
